60d8187b356edd023da6d447f910d489a6747467
BlisS <contacto@fixter.org>
2015-12-12 13:28:26 -0600

    listos

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
      url(r'^$', views.GetCursosView.as_view(), name='get_cursos'),
      url(r'^cursos/(?P<id>\d+)',views.ShowCurso.as_view(),name='show_curso'),
      url(r'^curso/(?P<id>\d+)/(?P<id_clase>\d+)',views.ShowClase.as_view(),name='show_clase'),
+     url(r'^pago/',views.Pagos.as_view(),name='pago'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.shortcuts import get_object_or_404, redirect
 from .models import Curso,Tema,Clase
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -52,3 +52,7 @@ class ShowClase(View):
 		"clases":clases,
 		}
 		return render(request,template,context)
+
+
+class Pagos(TemplateView):
+	template_name = "cursos/pago.html"
